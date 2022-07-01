8351cae8c6b0db9fd5f31406cfd5933bcf472ebf
jaspinderpawar <jaspinder@founderandlightning.com>
2022-07-01 16:28:15 +0530

    dfdf

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 
 var express = require("express");
-//var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 var app = express();
 
+const url = process.env.MONGODB_URL;
+const options = {
+  useNewUrlParser: true,
+  autoIndex: true,
+  useUnifiedTopology: true,
+};
+
+console.log(url)
+
 app.listen(5001, () => {
-  console.log("Server running on port 5001");
+  console.log("Server running on the port 5001");
 });
 
-// const url = process.env.MONGODB_URL;
-// const options = {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   autoIndex: true,
-//   useUnifiedTopology: true,
-// };
 
 // mongoose.connect(url, options).then(() => {
 //   console.log("Connected to MongoDB");
